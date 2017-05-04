2dfa036634ea4cae806b5b94ec92911e580b4d10
Rohit Kumar <r.kumar002@studenti.unibs.it>
2017-05-04 22:37:02 +0200

    Create Face.java

---
+++ Face.java
@@ -0,0 +1,18 @@
+public class FacePrinter {
+//stampa una faccia
+	public static void main(String[] args) {
+		System.out.println("      O     ");
+		System.out.println("      |     ");
+		System.out.println("      |     ");
+		System.out.println(" ____|||___ ");	
+		System.out.println(" |        | ");
+		System.out.println("(| 0    0 |)");
+		System.out.println(" |        | ");
+		System.out.println(" |   __   | ");
+		System.out.println(" .......... ");
+		System.out.println("   ......   ");
+		System.out.println("     ..     ");
+
+	}
+
+}
